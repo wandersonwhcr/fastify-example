c98ccdc8350229002735e24d8f40140ddf1deb0d
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-08-24 20:52:24 -0300

    Adiciona novos álbuns

--- README.md
+++ README.md
@@ -53,3 +53,21 @@ curl --include --request DELETE \
 HTTP/1.1 204 No Content
 Date: Tue, 24 Aug 2021 23:30:14 GMT
 ```
+
+## Álbuns
+
+### Adicionar
+
+```
+curl --include --request POST \
+    --header 'Content-Type: application/json' \
+    --data '{"artist": {"_id": "452fae7f-7945-4442-bfc6-d59253f668cf"}, "name": "Holy Diver"}' \
+    http://albums.localhost/v1/albums
+
+HTTP/1.1 201 Created
+Content-Length: 46
+Content-Type: application/json; charset=utf-8
+Date: Tue, 24 Aug 2021 23:16:44 GMT
+
+{"_id":"452fae7f-7945-4442-bfc6-d59253f668cf"}
+```
--- services/albums/index.js
+++ services/albums/index.js
@@ -1,5 +1,193 @@
+const { Binary } = require('mongodb');
+const uuid = require('uuid');
+
+class UUID extends Binary {
+  constructor(buffer) {
+    super(buffer, Binary.SUBTYPE_UUID);
+  }
+  static fromBinary(content) {
+    return new UUID(content.buffer);
+  }
+  static fromString(content) {
+    return new UUID(uuid.parse(content));
+  }
+  toString() {
+    return uuid.stringify(this.buffer);
+  }
+}
+
 const fastify = require('fastify')({
   logger: true,
+});
+
+fastify.register(require('fastify-mongodb'), {
+  forceClose: true,
+  url: process.env.MONGODB_URL,
+});
+
+fastify.get('/', {
+  schema: {
+    type: 'object',
+    required: ['name', 'version'],
+    properties: {
+      name: { type: 'string' },
+      version: { type: 'string' },
+    },
+  },
+  handler: async function (req, res) {
+    res.status(200)
+      .send({
+        name: process.env.npm_package_name,
+        version: process.env.npm_package_version,
+      });
+  },
+});
+
+// LISTAR ALBUNS ===================================================================================
+
+fastify.get('/v1/albums', {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          required: ['_id', 'name', 'artist'],
+          properties: {
+            _id: { type: 'string', format: 'uuid' },
+            name: { type: 'string' },
+            artist: {
+              type: 'object',
+              required: ['_id'],
+              properties: {
+                _id: { type: 'string', format: 'uuid' },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  handler: async function (req, res) {
+    const result = await this.mongo.db.collection('albums').find().toArray();
+
+    result.forEach((element) => {
+      element._id = UUID.fromBinary(element._id);
+      element.artist._id = UUID.fromBinary(element.artist._id);
+    });
+
+    res.status(200)
+      .send(result);
+  },
+});
+
+// ADICIONAR ALBUM =================================================================================
+
+fastify.post('/v1/albums', {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['name', 'artist'],
+      properties: {
+        name: { type: 'string' },
+        artist: {
+          type: 'object',
+          required: ['_id'],
+          properties: {
+            _id: { type: 'string', format: 'uuid' },
+          },
+        },
+      },
+    },
+    response: {
+      201: {
+        type: 'object',
+        required: ['_id'],
+        properties: {
+          _id: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: async function (req, res) {
+    const _id = UUID.fromString(uuid.v4());
+
+    req.body.artist._id = UUID.fromString(req.body.artist._id);
+
+    await this.mongo.db.collection('albums')
+      .insertOne({ _id, ...req.body });
+
+    res.status(201)
+      .send({ _id });
+  },
+});
+
+// ATUALIZAR ALBUM =================================================================================
+
+fastify.put('/v1/albums/:_id', {
+  schema: {
+    params: {
+      _id: { type: 'string', format: 'uuid' },
+    },
+    body: {
+      type: 'object',
+      required: ['name', 'artist'],
+      properties: {
+        name: { type: 'string' },
+        artist: {
+          type: 'object',
+          required: ['_id'],
+          properties: {
+            _id: { type: 'string', format: 'uuid' },
+          },
+        },
+      },
+    },
+  },
+  handler: async function (req, res) {
+    const _id = UUID.fromString(req.params._id);
+
+    req.body.artist._id = UUID.fromString(req.body.artist._id);
+
+    const result = await this.mongo.db.collection('albums')
+      .updateOne({ _id }, { $set: req.body });
+
+    if (result.matchedCount === 0) {
+      res.status(404)
+        .send();
+
+      return;
+    }
+
+    res.status(204)
+      .send();
+  },
+});
+
+// REMOVER ALBUM ===================================================================================
+
+fastify.delete('/v1/albums/:_id', {
+  schema: {
+    params: {
+      _id: { type: 'string', format: 'uuid' },
+    },
+  },
+  handler: async function (req, res) {
+    const _id = UUID.fromString(req.params._id);
+
+    const result = await this.mongo.db.collection('albums')
+      .deleteOne({ _id });
+
+    if (result.deletedCount === 0) {
+      res.status(404)
+        .send();
+
+      return;
+    }
+
+    res.status(204)
+      .send();
+  },
 });
 
 fastify.listen(process.env.PORT, '0.0.0.0');
