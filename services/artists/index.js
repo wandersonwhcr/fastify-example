eb7024e79fe49148f572d8d81b748a510ac0312a
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-08-24 19:29:51 -0300

    Adiciona rota principal de informações

--- services/artists/index.js
+++ services/artists/index.js
@@ -2,4 +2,27 @@ const fastify = require('fastify')({
   logger: true,
 });
 
+fastify.register(require('fastify-mongodb'), {
+  forceClose: true,
+  url: process.env.MONGODB_URL,
+});
+
+fastify.get('/', {
+  schema: {
+    type: 'object',
+    required: ['name', 'version'],
+    parameters: {
+      name: { type: 'string' },
+      version: { type: 'string' },
+    },
+  },
+  handler: async function (req, res) {
+    res.status(200)
+      .send({
+        name: process.env.npm_package_name,
+        version: process.env.npm_package_version,
+      });
+  },
+});
+
 fastify.listen(process.env.PORT, '0.0.0.0');
